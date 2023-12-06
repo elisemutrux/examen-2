9d5c08d4bff40e9dca6f0a37e2796b6e23fccf79
elisemutrux <122873450+elisemutrux@users.noreply.github.com>
2023-12-06 11:39:19 +0100

    burger menu de base

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var menu = document.querySelector(".menu");
 
 var toggleMenu = function toggleMenu() {
   menu.classList.toggle("is-active");
+  burger.classList.toggle("is-active");
 };
 
 burger.addEventListener("click", toggleMenu);
