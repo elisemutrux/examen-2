f3399dd2f909410f1b6b244d69a8b11d5a4dce72
elisemutrux <122873450+elisemutrux@users.noreply.github.com>
2023-12-06 12:07:24 +0100

    burger menu avancé

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 var burger = document.querySelector(".burger");
 var menu = document.querySelector(".menu");
+var backgroundBurgerMenu = document.querySelector(".background-burger-menu");
 
 var toggleMenu = function toggleMenu() {
   menu.classList.toggle("is-active");
   burger.classList.toggle("is-active");
+  backgroundBurgerMenu.classList.toggle("is-active");
 };
 
 burger.addEventListener("click", toggleMenu);
+backgroundBurgerMenu.addEventListener("click", toggleMenu);
